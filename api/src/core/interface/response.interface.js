ba169f7775ae44f1981c29966585835d9ca41318
LukaPatarcic <luka.patarcic@auxality.com>
2022-04-18 11:19:45 +0200

    Removed js files

--- api/src/core/interface/response.interface.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// success: true => message, data
-// success: false => errorMessage, error
-exports.__esModule = true;
--- api/src/features/auth/get-user.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.GetUser = void 0;
-var common_1 = require("@nestjs/common");
-exports.GetUser = (0, common_1.createParamDecorator)(function (data, ctx) {
-    var request = ctx.switchToHttp().getRequest();
-    return request.user;
-});
